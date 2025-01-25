dd14cc16c907fa7f2d406974fb4df1c132a202b1
thedraculaisreal <79720230+thedraculaisreal@users.noreply.github.com>
2025-01-24 17:30:53 -0800

    gonna flesh this out tommorw, just testing a coupel of tings

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 use proc_mem::Process;
+use std::thread;
+use std::time::Duration;
+
+mod offsets;
+mod entities;
 
 fn main() {
     let game = Process::with_name("cs2.exe").expect("Failed to find game");
     let client = game.module("client.dll").unwrap();
     println!("0x{:x}", client.base_address());
+    let local_player_address: usize = game.read_mem::<usize>(client.base_address() + offsets::LOCAL_PLAYER)
+	.expect("failed to read local_player address");
+    loop {
+	let health: i32 = game.read_mem::<i32>(local_player_address + 0x344)
+	    .expect("failed to read health value");
+	println!("{health}");
+	thread::sleep(Duration::from_millis(1000));
+    }
 }
---
+++ src/offsets.rs
@@ -0,0 +1,2 @@
+pub static ENTITY_LIST: usize = 0x1A197E8;
+pub static LOCAL_PLAYER: usize = 0x186DE00;
