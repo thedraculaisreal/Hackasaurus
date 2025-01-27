8ad6178dd7b3dc889a44a8725f9dba1a36522948
thedraculaisreal <79720230+thedraculaisreal@users.noreply.github.com>
2025-01-26 16:38:45 -0800

    fixed the entity_list loop

--- src/entities.rs
+++ src/entities.rs
@@ -9,15 +9,15 @@ pub fn entity_list_loop(client: proc_mem::Module, server: proc_mem::Module, game
 	    .expect("failed to read entity_list address");
 	let player_count = game.read_mem::<i32>(server.base_address() + offsets::PLAYER_COUNT)
 	    .expect("failed to read player_count");
-	println!("{player_count}");
-	for i in 1..=player_count {
-	    let list_entry = game.read_mem::<usize>(entity_list + (((8 * (i & 0x7FFF) >> 9) + 16) as usize))
+	let mut player_index: i32 = 1;
+	while player_index <= player_count {
+	    let list_entry = game.read_mem::<usize>(entity_list + (((8 * (player_index & 0x7FFF) >> 9) + 16) as usize))
 		.expect("failed to read list_entry");
-	    let entity_controller = game.read_mem::<usize>(list_entry + ((120 * (i & 0x1FF)) as usize))
+	    let entity_controller = game.read_mem::<usize>(list_entry + ((120 * (player_index & 0x1FF)) as usize))
 		.expect("failed to read entity_controller");
-	    let player_pawn = game.read_mem::<usize>(entity_controller + offsets::PLAYER_PAWN)
+	    let player_pawn = game.read_mem::<u32>(entity_controller + offsets::PLAYER_PAWN)
 		.expect("failed to read player_pawn");
-	    let list_entity = game.read_mem::<usize>(entity_list + ((0x8 * ((player_pawn & 0x7FFF) >> 9) + 16) as usize))
+	    let list_entity = game.read_mem::<usize>(entity_list + ((8 * ((player_pawn & 0x7FFF) >> 9) + 16) as usize))
 		.expect("failed to read list entity");
 	    let pawn_addr = game.read_mem::<usize>(list_entity + ((120 * (player_pawn & 0x1FF)) as usize))
 		.expect("failed to read pawn_addr");
@@ -25,6 +25,7 @@ pub fn entity_list_loop(client: proc_mem::Module, server: proc_mem::Module, game
 		.expect("failed to read pawn_health");
 	    println!("{}", pawn_health);
 	    thread::sleep(Duration::from_millis(1000));
+	    player_index += 1;
 	}
     }
 }
